2fde7b11d99b85d269ac6d644535f3ffb9ac5e63
Darryl White <whitedarryl101@gmail.com>
2017-04-03 11:36:00 +0100

    Initial commit

---
+++ routes/landing-page.js
@@ -0,0 +1,29 @@
+var request = require('request');
+
+var routes = {
+    init: function(app) {
+
+        // set up landing page
+        app.get('/', function (req, res, next) {
+
+            request('http://127.0.0.1:3000/api/products', function(error, response, body) {
+                res.render('index', {
+                    metadata: {
+                        title: 'NAP Tech Test'
+                    },
+                    title: 'NAP Tech Test',
+                    layout: 'default',
+                    template: 'index',
+                    products: body
+                });
+            });
+        });
+
+    }
+};
+
+
+
+module.exports = {
+    routes: routes
+};
